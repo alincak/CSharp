62348695546f23e89e802d00496647d26d622558
alincak <kerem1609@gmail.com>
2019-12-21 13:36:39 +0300

    ifade

---
+++ Ifade/Ifade/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ifade
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var a = 0;
+      var b = 0;
+
+      var c = a + b;
+
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
