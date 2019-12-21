e18fa7f1e0e3476d90750eeae6fc0a2b46b8f7dc
alincak <kerem1609@gmail.com>
2019-12-21 18:58:43 +0300

    metotlar

---
+++ Metotlar/Metotlar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Metotlar
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Yaz();
+
+      Console.WriteLine(Pozitif(-1));
+
+      Console.ReadKey();
+    }
+
+    public static void Yaz()
+    {
+      Console.WriteLine("Yazdım.");
+    }
+
+    public static string Pozitif(int x)
+    {
+      return x > 0 
+        ? "Pozitif" 
+        : "Negatif";
+    }
+
+  }
+}
