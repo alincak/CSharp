dae1621abd61bfc70d4769b4f5a3089508fb654f
alincak <kerem1609@gmail.com>
2019-12-21 13:27:15 +0300

    bildirim

---
+++ Bildirim/Bildirim/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Bildirim
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //değişken isimleri küçük;
+
+      //int tipinden
+      int a; //geçerli
+
+      int b = 10;
+
+      bool x, y = false;
+
+      bool _x = false;
+
+      //double 1a; //error
+
+      float çeşme;
+    }
+  }
+
+  class Sample
+  {
+
+    void Test()
+    {
+      {
+        int a = 10;
+      }
+      
+      a = 15; //error
+    }
+  }
+
+}
