aeb6e458dccd8f8d049feac9e9c3efb6a02ced69
alincak <kerem1609@gmail.com>
2019-12-21 18:17:04 +0300

    operator

---
+++ Operatorler/Operatorler/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Operatorler
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var a = 10;
+      var b = 20;
+
+      //Aritmetik */+-
+      var x = a + b;
+      x = a * b;
+      x = a / b;
+      x = a - b;
+
+
+      //Karşılaştırma < > <= >= ==
+      var y = a < b;
+      y = a > b;
+      y = a >= b;
+      y = a <= b;
+      y = a == b;
+
+      //üç operandlı ?:
+      var z = a == 10 
+        ? a + 1 
+        : a - 1;
+
+
+      //Tek operandlı !
+      var zz = !y;
+
+      //iki operandlı || &&
+      var yz = zz || y;
+      yz = zz && y;
+
+    }
+  }
+}
