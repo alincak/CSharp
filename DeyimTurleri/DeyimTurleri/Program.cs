aab7d853546080452a3b576c851d17ae9798588e
alincak <kerem1609@gmail.com>
2019-12-21 18:42:02 +0300

    deyimturleri

---
+++ DeyimTurleri/DeyimTurleri/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DeyimTurleri
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //a + b;
+
+      //iç içe deyimler
+      {
+        {
+          int b;
+        }
+
+        int a;
+      }
+      int c;
+
+      //boş deyim
+      int aa;;
+
+      //bildirim deyimi
+      int x;
+
+      //kontrol deyimler if, for, while, do while
+
+    }
+  }
+}
