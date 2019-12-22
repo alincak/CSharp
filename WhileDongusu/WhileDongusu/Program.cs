addf27c7084b630bf915914d142c453bcb79e4f0
alincak <kerem1609@gmail.com>
2019-12-22 18:12:56 +0300

    while

---
+++ WhileDongusu/WhileDongusu/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WhileDongusu
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Bir sayı giriniz:");
+
+      var x = int.Parse(Console.ReadLine());
+
+      //Yaz(x);
+      Yaz1(x);
+
+      Console.ReadKey();
+    }
+
+    static void Yaz(int x)
+    {
+      while (x > 0)
+      {
+        Console.WriteLine(x);
+        --x;
+      }
+    }
+
+    static void Yaz1(int x)
+    {
+      while (x > 0)
+        Console.WriteLine(x--);
+    }
+
+  }
+}
