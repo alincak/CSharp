f17d612210828c54a54c0f094e59ddae14385d70
alincak <kerem1609@gmail.com>
2019-12-22 18:22:05 +0300

    dowhile

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DoWhile
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Iste();
+    }
+
+    static void Iste()
+    {
+      var x = 0;
+      do
+      {
+        Console.Write("Çift sayı girin:");
+        x = int.Parse(Console.ReadLine());
+      } while (x % 2 != 0);
+    }
+  }
+}
