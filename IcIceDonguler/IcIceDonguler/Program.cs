b2716b5af9436efe26fe5436cb3e9fc264d72235
alincak <kerem1609@gmail.com>
2019-12-22 19:04:32 +0300

    icicedongu

---
+++ IcIceDonguler/IcIceDonguler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IcIceDonguler
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Yaz();
+    }
+
+    static void Yaz()
+    {
+      for (var i = 0; i < 10; i++)
+      {
+        for (var k = 0; k < 2; k++)
+        {
+          var text = string.Format("i: {0}, k: {1}", i, k);
+          Console.WriteLine(text);
+        }
+      }
+
+      Console.ReadKey();
+    }
+
+  }
+}
