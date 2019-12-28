24136706991c0b1fe82b531581a9eb24bfa3627b
alincak <kerem1609@gmail.com>
2019-12-28 11:55:13 +0300

    goto

---
+++ Goto/Goto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Goto
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Bir sayı girin: ");
+      var sayi = int.Parse(Console.ReadLine());
+
+      for (var i = 0; i < sayi; i++)
+      {
+        Console.WriteLine("Sayi: " + i);
+        if (i == 8)
+        {
+          goto Found;
+        }
+      }
+
+    Found:
+      Console.WriteLine("Buldum.");
+
+      Console.ReadKey();
+    }
+  }
+}
