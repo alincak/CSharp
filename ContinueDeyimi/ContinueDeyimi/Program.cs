500379ab6a329eabc7ea37c355432611e52762e9
alincak <kerem1609@gmail.com>
2019-12-28 11:46:46 +0300

    continue deyimi

---
+++ ContinueDeyimi/ContinueDeyimi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ContinueDeyimi
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Bir sayı girin: ");
+      var sayi = int.Parse(Console.ReadLine());
+
+      for (var i = 0; i < sayi; i++)
+      {
+        if (i > 3)
+        {
+          continue;
+        }
+
+        Console.WriteLine(i);
+      }
+
+      Console.ReadKey();
+
+    }
+  }
+}
