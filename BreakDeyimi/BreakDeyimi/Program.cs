0f780dc53fa5a32f8977b86f1a0bef35c74e196a
alincak <kerem1609@gmail.com>
2019-12-28 11:37:21 +0300

    break

---
+++ BreakDeyimi/BreakDeyimi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BreakDeyimi
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+      char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+
+      for (var i = 0; i < numbers.Length; i++)
+      {
+        Console.WriteLine(string.Format("Sayi: {0}", numbers[i]));
+        for (var k = 0; k < letters.Length; k++)
+        {
+          if (i == k)
+          {
+            break;
+          }
+          Console.Write(string.Format("{0}", letters[k]));
+        }
+        Console.WriteLine();
+      }
+
+      Console.ReadKey();
+
+    }
+  }
+}
