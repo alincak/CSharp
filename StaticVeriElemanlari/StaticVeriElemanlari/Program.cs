48ec620a600ff631fd2c0945af9c41d6cbb6567a
alincak <kerem1609@gmail.com>
2019-12-29 18:19:50 +0300

    static veri elamları

---
+++ StaticVeriElemanlari/StaticVeriElemanlari/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StaticVeriElemanlari
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(string.Format("En yüksek değer: {0}", RasgeleSayi.EnYuksek));
+
+      Console.ReadKey();
+    }
+  }
+
+  class RasgeleSayi
+  {
+    public static int EnYuksek = 100;
+
+    public int Olustur()
+    {
+      return 0;
+    }
+  }
+
+}
