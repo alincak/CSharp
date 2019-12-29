05f0d14d9c826df5cc94abb3a501590f49a6aba5
alincak <kerem1609@gmail.com>
2019-12-29 14:41:51 +0300

    value reference types

---
+++ ValueReferenceTypes/ValueReferenceTypes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ValueReferenceTypes
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //value type: byte, int,decimal, double, long
+      byte a = 5;
+      byte b = a;
+
+      Console.WriteLine(++b);
+      Console.WriteLine(a);
+
+      int c = 5;
+
+      //reference type: sınıflar
+      Test x = new Test();
+      Console.WriteLine("Eski x: " + x.a);
+
+      Test y = x;
+      y.a = 10;
+      Console.WriteLine("y: " + y.a);
+
+      Console.WriteLine("x: " + x.a);
+
+      Console.ReadKey();
+    }
+
+    class Test 
+    {
+      public int a = 5; 
+    }
+
+  }
+}
