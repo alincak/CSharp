3d3bbd6dc5fea30f8726f5b8be917a454a36b1aa
alincak <kerem1609@gmail.com>
2020-01-05 18:55:11 +0300

    string class

---
+++ StringClass/StringClass/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StringClass
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var text = " Belirtilen bir karakter konumunda başlayan ve dizenin sonunda sona eren bir dizeden bir alt dize çıkarmak için... ";
+      var sample = new Sample(text);
+
+      Console.WriteLine("GenerateText");
+      var t = Sample.GenerateText('a', 10);
+      Console.WriteLine(t);
+
+      Console.WriteLine();
+      Console.WriteLine("Display");
+      sample.Display();
+
+      Console.WriteLine();
+      Console.WriteLine("Remove");
+      sample.Remove(2, 5);
+
+      Console.WriteLine();
+      Console.WriteLine("IndexOf");
+      sample.IndexOf('t');
+
+      Console.WriteLine();
+      Console.WriteLine("Replace");
+      sample.Replace("bir", "iki");
+
+      Console.WriteLine();
+      Console.WriteLine("DisplayCharacters");
+      sample.DisplayCharacters(7, 8);
+
+      Console.WriteLine();
+      Console.WriteLine("DisplayCharacters");
+      sample.DisplayCharacters();
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ StringClass/StringClass/Sample.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringClass
+{
+  class Sample
+  {
+    private string _text;
+
+    public Sample(string text)
+    {
+      _text = string.IsNullOrEmpty(text)
+        ? string.Empty
+        : text.Trim();
+    }
+
+    private bool IsNullOrEmpty()
+    {
+      return string.IsNullOrEmpty(_text);
+    }
+
+    public static string GenerateText(char c, int count)
+    {
+      return new string(c, count);
+    }
+
+    public void Display()
+    {
+      Console.WriteLine(_text);
+    }
+
+    public void Remove(int startIndex, int count)
+    {
+      Console.WriteLine(string.Format("Eski metin: {0}", _text));
+      var newText = _text.Remove(startIndex, count);
+      Console.WriteLine(string.Format("Yeni metin: {0}", newText));
+    }
+
+    public void IndexOf(char value)
+    {
+      var index = _text.IndexOf(value);
+      Console.WriteLine(string.Format("'{0}' Index: {1}", value, index));
+    }
+
+    public void Replace(string oldValue, string newValue)
+    {
+      if (IsNullOrEmpty())
+      {
+        Console.WriteLine("Text is null or empty.");
+        return;
+      }
+
+      Console.WriteLine(string.Format("Eski metin: {0}", _text));
+      var newText = _text.Replace(oldValue, newValue);
+      Console.WriteLine(string.Format("Yeni metin: {0}", newText));
+    }
+
+    public void DisplayCharacters(int startIndex, int length)
+    {
+      var newText = _text.Substring(startIndex, length);
+      DisplayCharacters(newText);
+    }
+
+    public void DisplayCharacters()
+    {
+      DisplayCharacters(_text);
+    }
+
+    private void DisplayCharacters(string text)
+    {
+      for (int i = 0; i < text.Length; i++)
+      {
+        Console.WriteLine(text[i]);
+      }
+    }
+  }
+}
