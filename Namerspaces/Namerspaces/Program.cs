7a867c0f9e3b769aecf2b209189f908e7235bd2f
alincak <kerem1609@gmail.com>
2020-01-11 14:21:26 +0300

    namespaces

---
+++ Namerspaces/Namerspaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Namerspaces
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ Namespaces/Namespaces/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Namespaces
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      SampleA.B.SampleClass a;
+      SampleA.A.SampleClass aa;
+
+      SampleB.SampleClass b;
+    }
+  }
+}
+
+namespace SampleA.A
+{
+  class SampleClass
+  { 
+    
+  }
+}
+
+namespace SampleA.B
+{
+  class SampleClass
+  {
+
+  }
+}
+
+namespace SampleB
+{
+  class SampleClass
+  { 
+    
+  }
+}
