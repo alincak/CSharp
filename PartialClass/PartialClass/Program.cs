86bd681dfb123adc986a0762d277312884be70de
alincak <kerem1609@gmail.com>
2020-01-11 14:56:06 +0300

    partial class

---
+++ PartialClass/PartialClass/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PartialClass
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
+
+namespace SampleNested
+{
+  partial class Container
+  {
+    public void Test() { }
+  }
+
+  partial class Container
+  {
+    public void Test2() { }
+  }
+}
+
+namespace SampleNested2
+{
+  class Container
+  {
+    partial class Nested 
+    {
+      void Test() { }
+    }
+
+    partial class Nested
+    {
+      void Test2() { }
+    }
+  }
+}
+
+namespace SampleNested3
+{
+  partial class Container
+  {
+    partial class Nested { }
+  }
+
+  partial class Container
+  {
+    partial class Nested { }
+  }
+}
