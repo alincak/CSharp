5793a8417c051d1b0032e895fbd5ff145725c7cf
alincak <kerem1609@gmail.com>
2020-01-11 15:03:17 +0300

    nested class

---
+++ Nested/Nested/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Nested
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var sample = new Sample();
+
+      Console.WriteLine($"Sample X: { sample.X }");
+      Console.WriteLine($"NestedSample X: { sample.NestedSampleX }");
+
+      Console.ReadKey();
+    }
+  }
+
+  class Sample
+  { 
+    public int X { get; private set; }
+    public int NestedSampleX { get; private set; }
+
+    public Sample()
+    {
+      X = 5;
+      NestedSampleX = new NestedSample().X;
+    }
+
+    class NestedSample
+    { 
+      public int X { get; set; }
+
+      public NestedSample()
+      {
+        X = 10;
+      }
+    }
+  }
+
+}
