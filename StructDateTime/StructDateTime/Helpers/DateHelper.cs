88cec69745f89d8a8c234ed0a5d475d275c26fb6
alincak <kerem1609@gmail.com>
2020-01-12 17:38:05 +0300

    struct datetime

---
+++ StructDateTime/StructDateTime/Helpers/DateHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace StructDateTime.Helpers
+{
+  public static class DateHelper
+  {
+    public static int CalculateAge(DateTime date)
+    {
+      var now = DateTime.Now;
+      var age = now.Year - date.Year;
+
+      if (date > now.AddYears(-age))
+      {
+        age--;
+      }
+
+      return age;
+    }
+
+    public static void WriteDateDiff(DateTime date, DateTime date2)
+    {
+      var totalDays = (date - date2).TotalDays;
+
+      var totalYears = Math.Truncate(totalDays / 365);
+      var totalMonths = Math.Truncate((totalDays % 365) / 30);
+      var remainingDays = Math.Truncate((totalDays % 365) % 30);
+
+      Console.WriteLine();
+      Console.WriteLine($"{ totalYears } Yıl, { totalMonths } Ay, { remainingDays } Gün kalmış.");
+    }
+
+    public static string GetDayName(DateTime date, string cultureName = "tr-TR")
+    {
+      return CultureInfo.GetCultureInfo(cultureName).DateTimeFormat.DayNames[(int)date.DayOfWeek];
+    }
+
+  }
+}
---
+++ StructDateTime/StructDateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using StructDateTime.Helpers;
+using System;
+
+namespace StructDateTime
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var now = DateTime.Now;
+      var date = new DateTime(1071, 08, 26);
+      var date2 = new DateTime(2023, 10, 29);
+
+      Console.WriteLine($"Kaç yaşında: { DateHelper.CalculateAge(date) }. yıl");
+
+      Console.WriteLine();
+      Console.WriteLine($"Gün adı: { DateHelper.GetDayName(date) }");
+
+      DateHelper.WriteDateDiff(date2, now);
+    }
+  }
+}
