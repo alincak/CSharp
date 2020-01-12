f308ed69335cb7a1f844eee9064bb3bbd5d54f4b
alincak <kerem1609@gmail.com>
2020-01-12 17:15:31 +0300

    structures

---
+++ Structures/Structures/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Structures
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var customDateTime1 = new CustomDateTime();
+      var customDateTime2 = new CustomDateTime(30, 03, 1432);
+      CustomDateTime customDateTime3;
+
+      customDateTime3.Day = 20;
+      customDateTime3.Month = 01;
+      customDateTime3.Year = 1029;
+
+      Console.Write("customDateTime1: ");
+      Console.WriteLine($"Gün: { customDateTime1.Day }, Ay: { customDateTime1.Month }, Yıl: { customDateTime1.Year }");
+
+      Console.Write("customDateTime2: ");
+      Console.WriteLine($"Gün: { customDateTime2.Day }, Ay: { customDateTime2.Month }, Yıl: { customDateTime2.Year }");
+
+      Console.Write("customDateTime3: ");
+      Console.WriteLine($"Gün: { customDateTime3.Day }, Ay: { customDateTime3.Month }, Yıl: { customDateTime3.Year }");
+
+    }
+  }
+
+  struct CustomDateTime
+  {
+    public int Day, Month, Year;
+
+    public CustomDateTime(int day, int month, int year)
+    {
+      Day = day;
+      Month = month;
+      Year = year;
+    }
+
+    public void AddDays(int value)
+    {
+      Day += value;
+    }
+  }
+}
