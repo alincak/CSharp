978e9e9f3c2acabb291100a8739d04175a4eaca4
alincak <kerem1609@gmail.com>
2020-01-26 17:19:54 +0300

    dll

---
+++ DLL/DLL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GeometryCalculator;
+
+namespace DLL //Dynamic Link Library
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var circle = new Circle(10);
+
+      Console.WriteLine($"Alanı: { circle.Area }");
+      Console.WriteLine($"Çevresi: { circle.Circumference }");
+      Console.WriteLine($"Çapı: { circle.Diameter }");
+    }
+  }
+}
---
+++ DLL/GeometryCalculator/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GeometryCalculator
+{
+  public class Circle
+  {
+    private double m_radius { get; set; }
+
+    public Circle(double radius)
+    {
+      m_radius = radius;
+    }
+
+    public double Diameter
+    {
+      get { return 2 * m_radius; }
+    }
+
+    public double Circumference
+    {
+      get { return 2 * m_radius * Math.PI; }
+    }
+
+    public double Area
+    {
+      get
+      {
+        return Math.PI * Math.Pow(m_radius, 2);
+      }
+    }
+  }
+}
