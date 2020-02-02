dab37eb754f861c98d6bb6b9b7f9994606837854
alincak <kerem1609@gmail.com>
2020-02-02 16:08:01 +0300

    encapsulation

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,42 @@
+﻿using SampleDLL;
+
+namespace Encapsulation
+{
+            //Public
+            //Private
+            //Protected
+            //Internal
+            //Protected internal
+  
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var sClass = new SampleClass();
+      //sClass.m_text --- Hata
+      
+
+      var sBaseClass = new SampleBaseClass();
+      //sBaseClass.BaseText ---- Hata
+
+      var dllClass = new MyClassInternal();
+      dllClass.Sample();
+    }
+  }
+
+  class SampleClass : SampleBaseClass
+  { 
+    private string m_text { get; set; }
+
+    public string Text
+    {
+      get { return m_text + BaseText; }
+      set { m_text = value; }
+    }
+  }
+
+  class SampleBaseClass
+  { 
+    protected string BaseText { get; set; }
+  }
+}
---
+++ Encapsulation/SampleDLL/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SampleDLL
+{
+  public class MyClass
+  {
+    internal string MyText { get; set; }
+  }
+
+  public class MyClassInternal
+  {
+    public void Sample()
+    {
+      var myClass = new MyClass();
+      myClass.MyText = "";
+
+      var piClass = new MyClassProtectedInternal();
+      piClass.PI = "";
+    }
+  }
+
+  public class MyClassProtectedInternal
+  { 
+    protected internal string PI { get; set; }
+  }
+}
