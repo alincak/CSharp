2a2bdf6b3a85cb5c70a97c711fa80ee8dd631498
alincak <kerem1609@gmail.com>
2020-02-23 16:47:49 +0300

    composition

---
+++ Composition/Composition/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Composition
+{
+  class Program
+  {
+    //İçerilen nesne tek bir nesne tarafından içerilmelidir. Birden fazla nesne tarafından içirilemez
+    //İçerilen nesne ile içeren nesnenin ömürleri yaklaşık aynıdır
+
+    static void Main(string[] args)
+    {
+      {
+        var _b = new B();
+      }
+    }
+  }
+
+  class A
+  { 
+    //...
+  }
+
+  class B
+  {
+    private A m_a { get; set; }
+
+    public B()
+    {
+      m_a = new A();
+    }
+  }
+}
