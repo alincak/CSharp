e3ac3ba36bb27c54275589e44368b83e89957305
alincak <kerem1609@gmail.com>
2020-02-23 16:56:04 +0300

    Aggregation

---
+++ Aggregation/Aggregation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aggregation
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var _a = new A();
+
+      var _b = new B(_a);
+      var _c = new C(_a);
+    }
+  }
+
+  class A { }
+
+  class B
+  {
+    private A m_a { get; set; }
+    public B(A a)
+    {
+      m_a = a;
+    }
+  }
+
+  class C
+  {
+    private A m_a { get; set; }
+
+    public C(A a)
+    {
+      m_a = a;
+    }
+  }
+}
