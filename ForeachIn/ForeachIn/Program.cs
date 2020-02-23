c1ad157d850b2f74fa14d502e3bbd0902714aca9
alincak <kerem1609@gmail.com>
2020-02-23 15:45:49 +0300

    foreach

---
+++ ForeachIn/ForeachIn/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ForeachIn
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var arr = new int[5] { 1, 2, 3, 4, 5 };
+
+      Console.Write("Çıktı:");
+      for (var i = 0; i < arr.Length; i++)
+      {
+        Console.Write($" { arr[i] } ");
+      }
+
+      Console.WriteLine();
+
+      Console.Write("Çıktı:");
+      foreach (var item in arr)
+      {
+        Console.Write($" { item } ");
+      }
+    }
+  }
+}
