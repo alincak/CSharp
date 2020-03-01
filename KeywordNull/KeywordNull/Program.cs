8325b8eaa248b79fa6e8f5311b45e474f470c4a2
alincak <kerem1609@gmail.com>
2020-03-01 17:36:34 +0300

    null

---
+++ KeywordNull/KeywordNull/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace KeywordNull
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      MyClass mc;
+
+      //if (mc == null) { }
+
+      mc = null;
+      if (mc == null) { }
+
+      string s = null;
+      string t = string.Empty;
+
+      bool b = t.Equals(s);
+      Console.WriteLine(b);
+
+      int? c = null;
+      if (c == null)
+      {
+        Console.WriteLine("i null");
+      }
+
+      Console.WriteLine(c.GetValueOrDefault());
+    }
+  }
+
+  class MyClass { }
+}
