f30b80c98712e59b102e45fdfe1e8ea21ab646d1
alincak <kerem1609@gmail.com>
2020-03-01 18:45:00 +0300

    base

---
+++ KeywordBase/KeywordBase/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KeywordBase
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var b = new B();
+      b.Write();
+    }
+  }
+
+  class A
+  { 
+    protected string val { get { return "A"; } }
+  }
+
+  class B : A
+  {
+    private new string val { get { return "B"; } }
+
+    public void Write()
+    {
+      Console.WriteLine(val);
+      Console.WriteLine(base.val);
+    }
+  }
+}
