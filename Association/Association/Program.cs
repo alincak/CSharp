194045c754ebc89da80543ab48b349d9fd76e555
alincak <kerem1609@gmail.com>
2020-03-01 14:32:43 +0300

    a

---
+++ Association/Association/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Association
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var a = new A();
+      var b = new B();
+
+      b.Write(a);
+    }
+  }
+
+  class A
+  {
+    public void Write()
+    {
+      Console.Write("A Sınıfı");
+    }
+
+    public void WriteLine()
+    {
+      Console.WriteLine("A Sınıfı");
+    }
+  }
+
+  class B
+  {
+    public void Write(A a)
+    {
+      a.Write();
+    }
+
+    public void WriteLine(A a)
+    {
+      a.WriteLine();
+    }
+  }
+}
