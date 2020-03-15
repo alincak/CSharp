4711105ce8d6659b5191dee5bb9c87b887c28a7b
alincak <kerem1609@gmail.com>
2020-03-15 12:51:58 +0300

    abstract

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AbstractClass
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var square = new Square(10);
+      square.GetArea();
+      square.Write();
+    }
+  }
+
+  abstract class Shape
+  {
+    public abstract int GetArea();
+
+    public void Write()
+    {
+      Console.WriteLine("Write");
+    }
+  }
+
+  class Square : Shape
+  { 
+    private int Side { get; set; }
+
+    public Square(int n)
+    {
+      Side = n;
+    }
+
+    public override int GetArea()
+    {
+      return Side * Side;
+    }
+  }
+}
